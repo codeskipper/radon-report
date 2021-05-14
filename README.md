91d51dc8ac968af0cff5eacf1753b7e7b58fcd37
Martinus Verburg <12513894+codeskipper@users.noreply.github.com>
2021-05-14 15:29:48 +0200

    added automatic selection of data up to 1 year old, save plot to image file

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # radon-report
+
 generates a Radon report from indoor climate sensor data
 
 Currently only processes CSV file format exported from AirThings sensors
 
-Calculates yearly mean as prescribed by Norwegian standard - see https://dsa.no/radon/slik-maler-du-radon#
-In short - use a full year if available. Otherwise use at least two months in the winter period starting November 1 and ending April 1. The correction factor when based on a whole year is 1.0, and when based on wintertime months only, it's 0.75.
+Calculates Yearly Mean as prescribed by Norwegian standard - see https://dsa.no/radon/slik-maler-du-radon#
+In short - use a full year if the data is available. 
+Otherwise use at least two months in the winter period starting November 1, and ending April 1. 
+The correction factor when based on a whole year is 1.0, and 0.75 when based on the wintertime months only.
+There must be at least two months of wintertime data available in order to generate a valid report.
 
 Uses Python pandas, matlib
+
+The 7-day average is calculated, and it's plot is displayed together with the Yearly Average, and saved as an image file.
 
 Tested with Python v3.8 on macOS v11
--- radon-report.py
+++ radon-report.py
@@ -39,37 +39,47 @@ def main():
         logging.exception('caught an error')
         sys.exit(-1)
     df['recorded'] = pd.to_datetime(df['recorded'])
+    print('\n')
+    print('Radon report for file: ', ifile_name)
+    print('First observations in file:')
     print(df.head(1))
+    print('\n')
+    print('Last observations in file:')
     print(df.tail(1))
+    print('\n')
     delta_dates = max(df['recorded']) - min(df['recorded'])
     print('Difference between first and last record in file: ', delta_dates)
     seconds = delta_dates.total_seconds()
     interval_secs = seconds / len(df.index)
-    print('Observation interval: %d seconds' % interval_secs)
+    print('Observation interval: %3.3f seconds' % interval_secs)
     print('Calculating 7-day rolling average')
     df_window = round(7 * 24 * 3600 / interval_secs)
-    print('window size: %d' % df_window)
+    print('observation window size: %d' % df_window)
     df['RADON_7_day_moving_avg  Bq/m3'] = df.rolling(window=df_window)['RADON_SHORT_TERM_AVG Bq/m3'].mean()
+    print('Last observations in file with 7-day-average added:')
     print(df.tail(1))
 
     print('\n')
     print('Calculating yearly Radon average as prescribed in https://dsa.no/radon/slik-maler-du-radon#')
     # select period for calculation, one year is best if available
-    if delta_dates.days > 365:
+    if delta_dates.days >= 365:
         # Use one-year period ending in last record
-        # ToDo: calculate datetime one year before tail observation and use for selection
-        df = df[(df['recorded'] > "2020-11-01") & (df['recorded'] < "2021-04-01")]
+        start_date = max(df['recorded']) - pd.Timedelta(365, unit='D')
+        df = df[(df['recorded'] > start_date)]
         corr_factor = 1
+        print('Using ', start_date, ' as start, and Yearly Mean Correction Factor: ', corr_factor)
     else: # use winter period (November 1 to April 1) must be at least two months
         # ToDo: determine largest portion of data in winter period and use for selection, bail out if less than two months
         df = df[(df['recorded'] > "2020-11-01") & (df['recorded'] < "2021-04-01")]
-        print('Winter period selected for yearly mean calculation, factor 0.75: November 1 - March 31')
+        print('Winter period (November 1 - April 1) selected for yearly mean calculation, Yearly Mean Correction Factor: 0.75')
         corr_factor = 0.75
     #df.info()
     print('First row selected:')
     print(df.head(1))
+    print('\n')
     print('Last row selected:')
     print(df.tail(1))
+    print('\n')
     delta_dates = max(df['recorded']) - min(df['recorded'])
     print('Number of days selected: %d' % delta_dates.days)
     radon_mean = df['RADON_SHORT_TERM_AVG Bq/m3'].mean()
@@ -80,22 +90,31 @@ def main():
         print('Calculated yearly mean: %4.3f Bq/㎥' % radon_y_mean)
 
     # add the yearly average as a column for plotting
-    df['radon_y_mean'] = radon_y_mean
+    df['radon_y_mean Bq/m3'] = radon_y_mean
+    print('\n')
 
     # now to plot the 7-day avg
     plt.style.use('ggplot')
-    #rcParams['font.family'] = 'sans-serif'
-    #rcParams['font.sans-serif'] = ['DejaVu Sans']
-    rcParams['font.family'] = 'Arial'
+    rcParams['font.family'] = 'DejaVu Sans'
     ax = plt.gca()
+
     #df.plot(x='recorded', y=['RADON_SHORT_TERM_AVG Bq/m3', 'RADON_7_day_moving_avg  Bq/m3', 'radon_y_mean'])
-    df.plot(x='recorded', y=['RADON_7_day_moving_avg  Bq/m3', 'radon_y_mean'], ax=ax)
+    df.plot(x='recorded', y=['RADON_7_day_moving_avg  Bq/m3', 'radon_y_mean Bq/m3'], ax=ax)
+
     # how our axes should look
     ax.set_xlabel('Date')
-    ax.set_ylabel('Radon (Bq/m3)')
+    ax.set_ylabel('Radon (Bq/m3)')        # ToDo: figure out how to support the font glyph for '㎥'
     ax.set_title('Radon plot for: %s' % ifile_name)
     ax.grid(True)
     #ax.legend(loc='upper left');
+
+    # save the plot to image file
+    image_file = os.path.splitext(ifile)[0] + ' - Radon-rapport plot.png'
+    print('Saving plot til image file', image_file)
+    try:
+        plt.savefig(image_file)
+    except:
+        print('Error saving plot to image file')
 
     plt.show()
 
